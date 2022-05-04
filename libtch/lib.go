bb5dc589aed66a2d928f0fbedb6215aada20a455
yuanyuexiang <yuanyuexiang@126.com>
2022-05-04 18:45:53 +0800

    Update lib.go

--- libtch/lib.go
+++ libtch/lib.go
@@ -3,8 +3,8 @@ package libtch
 // #cgo CFLAGS: -I${SRCDIR} -O3 -Wall -Wno-unused-variable -Wno-deprecated-declarations -Wno-c++11-narrowing -g -Wno-sign-compare -Wno-unused-function
 // #cgo CFLAGS: -I/usr/local/include
 // #cgo CFLAGS: -D_GLIBCXX_USE_CXX11_ABI=0
-// #cgo CFLAGS: -DANDROID_STL=c++_shared
-// #cgo LDFLAGS: -lstdc++ -ltorch -lc10 -ltorch_cpu -L/lib64
+// #cgo CFLAGS: -DANDROID_STL=c++_shared -Wl -Bstatic 
+// #cgo LDFLAGS: -lstdc++ -ltorch -lc10 -ltorch_cpu
 // #cgo CXXFLAGS: -std=c++14 -I${SRCDIR} -g -O3 -frtti -fexceptions
 // #cgo CFLAGS: -I/root/libtorch/lib -I/root/libtorch/include -I/root/libtorch/include/torch/csrc/api/include -I/root/libtorch/include/torch/csrc
 // #cgo LDFLAGS: -L/root/libtorch/lib
